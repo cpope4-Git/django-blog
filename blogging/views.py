f22684e2a147b1208b6e759ebbd62ca8c6dba195
cpope4-Git <cpope4@uw.edu>
2024-12-07 13:40:01 -0800

    new push

--- blogging/views.py
+++ blogging/views.py
@@ -4,28 +4,28 @@ from blogging.models import Post
 from django.http import Http404
 from django.views.generic import ListView, DetailView, TemplateView
 
+
 class BlogTemplateView(TemplateView):
 
-    template_name = 'stub.html'
+    template_name = "stub.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['args'] = self.args
-        context['kwargs'] = self.kwargs
+        context["args"] = self.args
+        context["kwargs"] = self.kwargs
         return context
 
 
 class BlogListView(ListView):
 
     model = Post
-    queryset = Post.objects.order_by('-published_date')
-    template_name = 'blogging/list.html'
-    context_object_name = 'posts'
-
+    queryset = Post.objects.order_by("-published_date")
+    template_name = "blogging/list.html"
+    context_object_name = "posts"
 
 
 class BlogDetailView(DetailView):
 
     model = Post
-    template_name = 'blogging/detail.html'
-    pk_url_kwarg = 'post_id'
+    template_name = "blogging/detail.html"
+    pk_url_kwarg = "post_id"
