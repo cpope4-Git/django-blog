be7e97a4439f99914cbf590710d9bd070f1381f0
cpope4-Git <cpope4@uw.edu>
2024-12-15 13:09:24 -0800

    Reformatted with Black

--- polling/views.py
+++ polling/views.py
@@ -5,8 +5,9 @@ from polling.models import Poll
 
 
 def list_view(request):
-    context = {'polls': Poll.objects.all() }
-    return render(request, 'polling/list.html', context)
+    context = {"polls": Poll.objects.all()}
+    return render(request, "polling/list.html", context)
+
 
 def detail_view(request, poll_id):
     try:
@@ -14,12 +15,12 @@ def detail_view(request, poll_id):
     except Poll.DoesNotExist:
         raise Http404("Poll does not exist")
 
-    if request.method == 'POST':
-        if request.POST.get('vote') == "Yes":
+    if request.method == "POST":
+        if request.POST.get("vote") == "Yes":
             poll.score += 1
         else:
             poll.score -= 1
         poll.save()
 
-    context = {'poll': poll}
-    return render(request, 'polling/detail.html', context)
+    context = {"poll": poll}
+    return render(request, "polling/detail.html", context)
